ff5f35c243b69d6d06b07b968cf136b4a199fb53
abdelmalek <abdelmalek@gmail.com>
2024-02-26 15:45:15 +0000

    fix

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -2,40 +2,30 @@ const fs = require('fs');
 
 async function countStudents(path) {
   if (fs.existsSync(path)) {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
       fs.readFile(path, 'utf8', (err, data) => {
         if (err) {
-          reject(new Error('Cannot load the database'));
+          throw Error('Cannot load the database');
         }
-
-        const result = data
-          .split('\n')
-          .map((line) => line.split(','))
-          .filter((check, index) => index !== 0 && check.length > 3);
-
-        const newlis = result.map((data) => [data[0], data[3]]);
-        const fields = new Set(newlis.map((item) => item[1]));
-
-        const finalresult = {};
-        fields.forEach((data) => {
-          finalresult[data] = 0;
+        const result = [];
+        data.split('\n').forEach((data) => {
+          result.push(data.split(','));
         });
-
-        newlis.forEach((data) => {
-          finalresult[data[1]] += 1;
-        });
-
-        console.log(`Number of students: ${result.length}`);
-        
-        fields.forEach((data) => {
-          console.log(`Number of students in ${data}: ${finalresult[data]}. List: ${newlis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`);
-        });
-
-        resolve({ result, finalresult, newlis });
+        result.shift();
+        const newis = [];
+        result.forEach((data) => newis.push([data[0], data[3]]));
+        const fields = new Set();
+        newis.forEach((item) => fields.add(item[1]));
+        const final = {};
+        fields.forEach((data) => { (final[data] = 0); });
+        newis.forEach((data) => { (final[data[1]] += 1); });
+        console.log(`Number of students: ${result.filter((check) => check.length > 3).length}`);
+        Object.keys(final).forEach((data) => console.log(`Number of students in ${data}: ${final[data]}. List: ${newis.filter((n) => n[1] === data).map((n) => n[0]).join(', ')}`));
+        resolve(result, final, newis);
       });
     });
   }
-  throw new Error('Cannot load the database');
+  throw Error('Cannot load the database');
 }
 
 module.exports = countStudents;
