602620063d563d6ec46dbf0bda0cb2f006e62878
abdelmalek <abdelmalek@gmail.com>
2024-02-27 07:35:45 +0000

    fix

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -1,14 +1,10 @@
 const express = require('express');
 
 const app = express();
-const port = 1245;
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!\n');
+  res.send('Hello Holberton School!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
-});
-
+app.listen(1245, () => {});
 module.exports = app;
